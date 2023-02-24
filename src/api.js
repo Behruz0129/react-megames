d3cdf58495105b972093c1f44d8bb52be638c3ea
Behruz Berdiyev <behruz.berdiyev.0129@gmail.com>
2023-02-25 00:46:37 +0500

    Fetching Other Games

--- src/api.js
+++ src/api.js
@@ -27,9 +27,13 @@ const currentMonth = getCurrentMonth();
 const currentDay = getCurrentDay();
 const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
 const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
-// const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 // Popular Games
 const popular_games = `games?key=${process.env.REACT_APP_RAWG_API}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=20`;
+const upcoming_games = `games?key=${process.env.REACT_APP_RAWG_API}&dates=${nextYear}&ordering=-added&page_size=20`;
+const new_games = `games?key=${process.env.REACT_APP_RAWG_API}&dates=${lastYear},${currentDate}&ordering=-released&page_size=20`;
 
 export const popularGamesURL = () => `${base_url}${popular_games}`;
+export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
+export const newGamesURL = () => `${base_url}${new_games}`;
